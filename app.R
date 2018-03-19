8fe442cff775ddda6ede0d263ad11389849bfbeb
imadem <imadem@users.noreply.github.com>
2018-03-19 09:50:25 +0700

    Update app.R

--- app.R
+++ app.R
@@ -13,9 +13,9 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
    output$map <- renderLeaflet({
-    leaflet() %>% 
-  addProviderTiles("Stamen.Watercolor") %>% 
-  addProviderTiles("OpenWeatherMap.Rain") %>% 
+    leaflet() %>% addTiles() %>%
+  #addProviderTiles("Stamen.Watercolor") %>% 
+  #addProviderTiles("OpenWeatherMap.Rain") %>% 
   setView(lat = 113.9213,
           lng = 0.7893,
           zoom = 6)
